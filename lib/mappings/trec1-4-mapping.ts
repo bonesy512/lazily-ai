7ebce7b305a6b9921e3a75a57bd405d923337f0a
bonesy512 <kozmo51488@gmail.com>
2025-07-23 20:45:31 -0500

    feat: complete comprehensive mapping of TREC form fields

--- lib/mappings/trec1-4-mapping.ts
+++ lib/mappings/trec1-4-mapping.ts
@@ -1,7 +1,7 @@
 // lib/mappings/trec1-4-mapping.ts
 
-// This file translates our clean database fields to the messy PDF field names.
-// We will fill this out with all the necessary fields.
+// This file translates our clean database/CSV fields to the messy PDF field names.
+// This map is used by the PDF generation engine to correctly place data onto the form.
 
 export const trecFieldMap = {
   // Section 1: Parties
@@ -19,12 +19,88 @@ export const trecFieldMap = {
     knownAddress: 'Texas known as',
   },
 
-  // THE FIX IS HERE: Renamed 'earnestMoney' to 'price'
+  // Section 3: Sales Price
   price: {
-    amount: 'earnest money of',
-    titleCompany: 'insurance Title Policy issued by',
-    salesPrice: 'undefined_6', // Added from our mapping session
+    cashPortion: 'Text1',
+    financeAmount: 'Text2',
+    salesPrice: 'undefined_6',
   },
-  
-  // ... we will continue to add other fields here as we build the PDF generation logic
+
+  // Section 4: Financing Addenda Checkboxes
+  financing: {
+    thirdParty: 'Third Party Financing Addendum',
+    loanAssumption: 'Loan Assumption Addendum',
+    seller: 'Seller Financing Addendum',
+  },
+
+  // Section 5: Earnest Money
+  earnestMoney: {
+    amount: 'earnest money of',
+  },
+
+  // Section 6: Title Policy & Survey
+  titlePolicy: {
+    sellerPays: 'Sellers',
+    buyerPays: 'Buyer',
+    amendShortages: 'ii will be amended to read shortages in area at the expense of',
+  },
+  survey: {
+    sellerPays: 'Sellers_2',
+    buyerPays: 'Buyers expense no later',
+  },
+
+  // Section 7: Property Condition
+  propertyCondition: {
+    asIs: '1 Buyer accepts the Property As Is',
+    asIsWithRepairs: '2 Buyer accepts the Property As Is provided Seller at Sellers expense shall complete the',
+    repairsList: 'following specific repairs and treatments',
+  },
+
+  // Section 8: Brokers & Commissions
+  brokers: {
+    listingFirmName: 'Listing Broker Firm',
+    listingFirmLicense: 'License No_4',
+    listingAgentName: 'Listing Associates Name',
+    sellingAgentName: 'Selling Associates Name-1',
+  },
+  commission: {
+    listingBroker: 'when the Listing Brokers fee is received Escrow agent is authorized and directed to pay Other Broker from',
+    otherBroker: 'pay Other Broker from',
+  },
+
+  // Section 9: Closing
+  closing: {
+    date: 'A The closing of the sale will be on or before',
+  },
+
+  // Section 12: Settlement Expenses
+  settlement: {
+    sellerContribution: 'Buyers Expenses as allowed by the lender',
+  },
+
+  // Section 21: Notices
+  notices: {
+    buyerEmail: 'Email',
+    sellerEmail: 'Email_2',
+  },
+
+  // Section 22: Addenda Checkboxes
+  addenda: {
+    subjectTo: 'Addendum for Property Subject to',
+    sellerLease: 'Sellers Temporary Residential Lease',
+    buyerLease: 'Buyers Temporary Residential Lease',
+  },
+
+  // Section 23: Option Fee
+  optionFee: {
+    amount: 'acknowledged by Seller and Buyers agreement to pay Seller',
+    days: 'Buyer has paid Seller', // Note: This was identified as the field for days
+    creditToSales: 'will',
+  },
+
+  // Execution Block
+  execution: {
+    date: 'EXECUTED the',
+    monthYear: 'day of',
+  },
 };
