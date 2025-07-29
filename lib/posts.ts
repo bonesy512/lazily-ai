3295092d359f6ed4ed83b3cad2c84fa2f53e7cb8
Thomas Schustereit <bonesy@bonesydesign.com>
2025-07-29 15:18:42 -0500

    refactor(blog): sync data layer with UI components

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,28 +1,29 @@
-// lib/posts.ts
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 
-const postsDirectory = path.join(process.cwd(), 'posts')
+const postsDirectory = path.join(process.cwd(), 'posts');
 
-export function getPostSlugs() {
-  const fileNames = fs.readdirSync(postsDirectory)
-  return fileNames.map(fileName => fileName.replace(/\.mdx$/, ''))
+function getPostSlugs() {
+  return fs.readdirSync(postsDirectory).map(fileName => fileName.replace(/\.mdx$/, ''));
 }
 
-export function getPostBySlug(slug: string) {
-  const realSlug = slug.replace(/\.mdx$/, '')
-  const fullPath = path.join(postsDirectory, `${realSlug}.mdx`)
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
-  const { data, content } = matter(fileContents)
-
-  return { slug: realSlug, frontmatter: data, content }
+export function getPostData(slug: string) {
+  const fullPath = path.join(postsDirectory, `${slug}.mdx`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+  const { data, content } = matter(fileContents);
+  return { slug, frontmatter: data, content };
 }
 
-export function getAllPosts() {
-  const slugs = getPostSlugs()
-  const posts = slugs
-    .map(slug => getPostBySlug(slug))
-    .sort((post1, post2) => (post1.frontmatter.date > post2.frontmatter.date ? -1 : 1))
-  return posts
+export function getSortedPostsData() {
+  const slugs = getPostSlugs();
+  const allPosts = slugs
+    .map(slug => getPostData(slug))
+    .sort((post1, post2) => (post1.frontmatter.date > post2.frontmatter.date ? -1 : 1));
+  return allPosts;
+}
+
+export function getAllPostSlugs() {
+  const slugs = getPostSlugs();
+  return slugs.map(slug => ({ params: { slug } }));
 }
